39bedc21c65eff2bfc43cf04f5af6a9f9a157ff0
John Freeman <jfreeman08@gmail.com>
2019-07-01 20:56:52 -0500

    Add hexhash and binhash

---
+++ tests/test_hash.py
@@ -0,0 +1,51 @@
+# pylint: disable=missing-docstring
+
+from string import printable
+
+from hypothesis import assume, given
+from hypothesis.strategies import (
+    none,
+    booleans,
+    floats,
+    text,
+    lists,
+    dictionaries,
+    recursive,
+)
+
+from typeclasses.hash import hexhash
+
+json = recursive( # pylint: disable=invalid-name
+    none() | booleans() | floats() | text(printable),
+    lambda children: lists(children, 1) |
+    dictionaries(text(printable), children, min_size=1),
+)
+
+
+@given(json)
+def test_reflexive(value):
+    assert hexhash(value) == hexhash(value)
+
+
+@given(json, json)
+def test_injective(a, b):
+    assume(a != b)
+    assert hexhash(a) != hexhash(b)
+
+
+def test_dict_literal_order():
+    a = {'a': 1, 'b': 2}
+    b = {'b': 2, 'a': 1}
+    assert hexhash(a) == hexhash(b)
+
+
+def test_dict_insert_order():
+    a = {}
+    a['a'] = 1
+    a['b'] = 2
+
+    b = {}
+    b['b'] = 2
+    b['a'] = 1
+
+    assert hexhash(a) == hexhash(b)
---
+++ typeclasses/hash.py
@@ -0,0 +1,37 @@
+"""A method for content hashing values."""
+
+import hashlib
+import operator
+import pickle
+import typing as t
+
+from typeclasses import typeclass
+
+T = t.TypeVar('T')  # pylint: disable=invalid-name
+
+
+@typeclass(T)
+def add_bytes(stream, value: T):
+    """Serialize a value to a byte stream."""
+    stream.update(pickle.dumps(value))
+
+
+@add_bytes.instance(t.Mapping, protocol=True)
+def _add_bytes_mapping(stream, mapping):
+    # We have to canonicalize the order of keys.
+    items = sorted(mapping.items(), key=operator.itemgetter(0))
+    for k, v in items:
+        add_bytes(stream, k)
+        add_bytes(stream, v)
+
+
+def binhash(value, algorithm=hashlib.sha256):
+    stream = algorithm()
+    add_bytes(stream, value)
+    return stream.digest()
+
+
+def hexhash(value, algorithm=hashlib.sha256):
+    stream = algorithm()
+    add_bytes(stream, value)
+    return stream.hexdigest()
